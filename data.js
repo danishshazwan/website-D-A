2a16056e4a24afaa90b51163ad06b8bd65233f86
dilaila754-dev <dilaila754@gmail.com>
2026-03-29 21:33:11 +0800

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-var filenames = ["pearlss.jpg", "holding.jpg", "toledo.jpg"];
-var quantities = [4, 1, 2];
-var prices = [80, 125, 75];
-
-var titles = [
-    "Girl with a Pearl Earring",
-    "Artist Holding a Thistle",
-    "Portrait of Eleonora of Toledo"
-];
